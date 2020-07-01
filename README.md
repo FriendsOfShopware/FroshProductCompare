74e9f50c47adf6b89cfce422c7e23e9e26077779
Thuong Le <thuong.le@nfq.asia>
2020-07-01 21:31:28 +0700

    First Released

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple Product Compare for Shopware 6
+
+## A plugin for [Shopware 6](https://github.com/shopware/platform) for Google Shopping
+
+## Requirements
+
+| Version 	| Requirements               	|
+|---------	|----------------------------	|
+| 1.0.0    	| Shopware 6.0 >=	            |
+
+
---
+++ src/SimpleProductCompare.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Justa\SimpleProductCompare;
+
+use Shopware\Core\Framework\Plugin;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class SimpleProductCompare extends Plugin
+{
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/DependencyInjection/'));
+        $loader->load('services.xml');
+    }
+}
